06b01d1bfac436ad8614f9fb74edeaf7250ecbce
cm <cksal5188@naver.com>
2019-07-08 17:45:34 +0900

    Delete gnu

--- tonghana_gnu/plugin/sns/facebook/tests/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$base = realpath(dirname(__FILE__) . '/..');
-require "../../../../../gnuboard5.3.1.6/plugin/sns/facebook/tests/$base/src/base_facebook.php";
-require "../../../../../gnuboard5.3.1.6/plugin/sns/facebook/tests/$base/src/facebook.php";
